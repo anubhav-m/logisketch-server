b91e34dc7d667375918449db80937bf81b885427
Anubhav Mishra <mishra.anubhav108@gmail.com>
2025-06-25 16:24:50 +0530

    completed generateImage controller w/Kroki service     - added utility function to extract code     - fixed some naming issues of engine     - generateImage controller functionality done

--- controllers/generateImage.controllers.js
+++ controllers/generateImage.controllers.js
@@ -1,11 +1,19 @@
 import { callLLM } from "../services/llm.services.js";
+import { callKroki } from "../services/kroki.services.js";
+import { extractCode } from "../utilities/extractCode.utilites.js";
 
 export const generateImage = async (req, res, next)=>{
 
     try{
         const code = await callLLM(req.body.prompt, req.body.model, req.body.engine);
+
+        const sanitisedCode = extractCode(code);
+        console.log(sanitisedCode[0]);
+
+        const svg = await callKroki(sanitisedCode[0], req.body.engine);
+        console.log(svg);
         
-        res.status(200).json({success: true, code});
+        res.status(200).json({success: true, code, svg});
     }
 
     catch(err){
---
+++ services/kroki.services.js
@@ -0,0 +1,25 @@
+export const callKroki = async (code, engine) => {
+    const response = await fetch("https://kroki.io/" , {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(
+            {
+                "diagram_source": code,
+                "diagram_type": engine,
+                "output_format": "svg"
+            }
+        )
+    })
+
+    if (!response.ok){
+        const errorText = await response.text();
+        const error = new Error(`Kroki Error #${response.status}: ${errorText}`);
+        error.statusCode = response.status;
+        throw error;
+    }
+
+    const svg = await response.text();
+    return svg;
+}
